a4b1b145302569343097db3b1c4662c77bdf06e5
mckahz <fletchusmaximus@gmail.com>
2025-07-28 10:25:32 +1000

    fixed empty strings

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-~/.cargo/bin/tree-sitter generate && ~/.cargo/bin/tree-sitter parse ../pac/test/basic/Task.pac
+~/.cargo/bin/tree-sitter generate && ~/.cargo/bin/tree-sitter parse ../pac/src/basics/String.pac
